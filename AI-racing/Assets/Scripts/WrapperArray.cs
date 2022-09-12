c214f2a20f9821680495d4bc2176e4424de0be38
Nick van Santen <vansantennick@gmail.com>
2022-09-12 14:34:03 +0200

    Fixed bug where multiple foreach loops would not work

--- AI-racing/Assets/Scripts/WrapperArray.cs
+++ AI-racing/Assets/Scripts/WrapperArray.cs
@@ -6,10 +6,10 @@ using System.Collections;
 /// </summary>
 /// <typeparam name="T"></typeparam>
 [Serializable]
-public class WrapperArray<T> : IEnumerator, IEnumerable
+public class WrapperArray<T> : IEnumerable
 {
     public T[] array;
-    private int position = -1;
+    public int Length { get { return array.Length; } }
 
     public WrapperArray() : this(new T[0])
     {
@@ -27,28 +27,42 @@ public class WrapperArray<T> : IEnumerator, IEnumerable
         set => array[index] = value;
     }
 
-    public int Length { get { return array.Length; } }
 
     // Set of functions required for IEnumerator and IEnumerable to make foreach work
     // https://docs.microsoft.com/en-us/troubleshoot/developer/visualstudio/csharp/language-compilers/make-class-foreach-statement
+    private class MyEnumerator: IEnumerator
+    {
+        public T[] array;
+        private int position = -1;
+
+        public MyEnumerator(T[] _array)
+        {
+            array = _array;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+        public bool MoveNext()
+        {
+            position++;
+            return (position < array.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public object Current
+        {
+            get { return array[position]; }
+        }
+    }
+
     public IEnumerator GetEnumerator()
     {
-        return (IEnumerator)this;
-    }
-
-    public bool MoveNext()
-    {
-        position++;
-        return (position < array.Length);
-    }
-
-    public void Reset()
-    {
-        position = -1;
-    }
-
-    public object Current
-    {
-        get { return array[position]; }
+        return new MyEnumerator(array);
     }
 }
