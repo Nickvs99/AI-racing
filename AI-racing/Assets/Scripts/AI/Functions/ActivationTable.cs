519d8bc71b601b91ccb7edd068e0d8f43819fe23
Nick van Santen <vansantennick@gmail.com>
2022-09-30 21:05:42 +0200

    Restructure activation functions

--- AI-racing/Assets/Scripts/AI/Functions/ActivationTable.cs
+++ AI-racing/Assets/Scripts/AI/Functions/ActivationTable.cs
@@ -7,7 +7,17 @@ public static class ActivationTable
     // then retrieve the corresponding function.
     public static Dictionary<string, Func<float, float>> table = new Dictionary<string, Func<float, float>>()
     {
-        {"Default", ActivationFunctions.Default},
-        {"Sigmoid", ActivationFunctions.Sigmoid},
+        {"Default", Default},
+        {"Sigmoid", Sigmoid},
     };
+
+    private static float Default(float x)
+    {
+        return x;
+    }
+
+    private static float Sigmoid(float x)
+    {
+        return 1f / (1f + (float)Math.Exp(-x));
+    }
 }
